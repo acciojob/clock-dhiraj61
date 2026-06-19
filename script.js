28be1a6517e32ce738a9a8ab6824bce7cbf2ed93
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-06-19 09:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let timer = document.getElementById('timer');
+
+setInterval(()=>{
+	let currentDate = new Date();
+	timer.textContent = (currentDate.getMonth() + 1) + '/' + (currentDate.getDate()) + '/' + (currentDate.getFullYear()) + ', ' + (currentDate.getHours()) + ':' + (currentDate.getMinutes()) + ':' + (currentDate.getSeconds()) + (currentDate.getHours() > 12 ? ' PM' : ' AM');
+},1000)
